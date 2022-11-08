fd5b0c7df79229fa3def75d0e03a96d872c3bbdb
simeon.liniger <simeon.liniger@hftm.ch>
2022-11-08 11:55:59 +0100

    Optimize request body

--- src/main/java/ch/rihssolutions/BlogDummyService.java
+++ src/main/java/ch/rihssolutions/BlogDummyService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 
 @RequestScoped
-public class BlogService {
+public class BlogDummyService {
 
     public List<Blog> getBlogs() {
         return List.of(new Blog("Hello from RESTEasy Reactive", "This is a blog post"));
--- src/main/java/ch/rihssolutions/BlogResource.java
+++ src/main/java/ch/rihssolutions/BlogResource.java
@@ -2,25 +2,30 @@ package ch.rihssolutions;
 
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+
 @Path("/blog")
 public class BlogResource {
 
-    @Inject
-    BlogService blogService;
+    // @Inject
+    // BlogDummyService blogService;
 
     @GET
-    public List<Blog> hello() {
+    public List<Blog> list() {
         return Blog.listAll();
     }
 
     @Transactional
     @POST
+    @Operation(description = "Add new Blog-Post.")
+    @RequestBody(content = @Content(mediaType = "application/json", example = "{ \"title\": \"title\", \"content\": \"content\" }"))
     public void addBlog(Blog blog) {
         blog.persist();
     }
